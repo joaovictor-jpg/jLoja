5213b235e565c2c86e80a7d4aff0133615874e5f
joao victor <joaovictormdasilva676@gmail.com>
2023-10-17 23:12:29 -0300

    Created a new jloja project and domain model

---
+++ jloja/src/main/java/com/joaovictorjpg/github/jloja/JlojaApplication.java
@@ -0,0 +1,13 @@
+package com.joaovictorjpg.github.jloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlojaApplication.class, args);
+	}
+
+}
---
+++ jloja/src/test/java/com/joaovictorjpg/github/jloja/JlojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovictorjpg.github.jloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
