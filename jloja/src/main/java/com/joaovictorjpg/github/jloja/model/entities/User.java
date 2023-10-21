48798136b430a62b83dabc7051990c3204344a35
joao victor <joaovictormdasilva676@gmail.com>
2023-10-20 21:34:53 -0300

    Order, Instant, ISO 8601

--- jloja/src/main/java/com/joaovictorjpg/github/jloja/model/entities/User.java
+++ jloja/src/main/java/com/joaovictorjpg/github/jloja/model/entities/User.java
@@ -3,6 +3,8 @@ package com.joaovictorjpg.github.jloja.model.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,6 +20,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User () {}
 
