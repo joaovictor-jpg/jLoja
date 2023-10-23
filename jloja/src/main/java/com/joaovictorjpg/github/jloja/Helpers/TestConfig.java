694330abb80dee0e9828ae20f777e6d9774359d6
joao victor <joaovictormdasilva676@gmail.com>
2023-10-23 19:02:57 -0300

    OrderItem, many-to-many association with extra attributes

--- jloja/src/main/java/com/joaovictorjpg/github/jloja/Helpers/TestConfig.java
+++ jloja/src/main/java/com/joaovictorjpg/github/jloja/Helpers/TestConfig.java
@@ -1,14 +1,8 @@
 package com.joaovictorjpg.github.jloja.Helpers;
 
-import com.joaovictorjpg.github.jloja.model.entities.Category;
-import com.joaovictorjpg.github.jloja.model.entities.Order;
-import com.joaovictorjpg.github.jloja.model.entities.Product;
-import com.joaovictorjpg.github.jloja.model.entities.User;
+import com.joaovictorjpg.github.jloja.model.entities.*;
 import com.joaovictorjpg.github.jloja.model.entities.enums.OrderStatus;
-import com.joaovictorjpg.github.jloja.model.repositories.CategoryRepository;
-import com.joaovictorjpg.github.jloja.model.repositories.OrderRepository;
-import com.joaovictorjpg.github.jloja.model.repositories.ProductRepository;
-import com.joaovictorjpg.github.jloja.model.repositories.UserRepository;
+import com.joaovictorjpg.github.jloja.model.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -69,5 +66,12 @@ public class TestConfig implements CommandLineRunner {
         p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
